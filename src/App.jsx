9ce934f4ee2866e2fc9e7a468734ab8230b5d8f5
Imad Ibrahim <imadibrahim164@gmail.com>
2024-05-07 19:29:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,309 @@
+import './App.css'
+import scanCodeImage from './assets/images/scan_code.png';
+import test from './assets/images/test.png';
+
+function App() {
+
+  return (
+    <>
+      <div className="TV_fixed">
+        <div className="right_div">
+          <div className="right_div_inside">
+
+            <h1> meet & greet</h1>
+            <h2>SAY 👋🏻 TO THIS AWESOME MAKERS</h2>
+            <h5>17 MAKERS IN NOW</h5>
+
+            <div className="right_div_inside1">
+              <div className='scan_box'>
+                <img src={scanCodeImage} alt="Example" />
+                <div className="scan_box1">
+                  <p>DON’T SEE YOUR NAME?</p>
+                  <h2>
+                    SCAN TO CHECK IN
+                  </h2>
+                </div>
+              </div>
+
+              <p>LAST UPDATED<span> 12 MIN AGO</span></p>
+
+            </div>
+
+          </div>
+        </div>
+
+        {/* ############################################################### */}
+        <div className="left_div">
+          {/* card  */}
+
+          <div className='card_row_1'>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <span className="mentor_tag">MENTOR</span>
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+
+              <div className="card_box">
+                <img src={test} alt="Example" />
+                <span className="mentor_tag">MENTOR</span>
+                <div className="name_text">
+                  <h3>DARLENE <br /> ROBERTSON </h3>
+                  <p>OUT IN 2 MIN</p>
+                </div>
+              </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <span className="mentor_tag">MENTOR</span>
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div> 
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <span className="mentor_tag">MENTOR</span>
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+          </div>
+
+
+
+
+          <div className='card_row_1'>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+          </div>
+
+   
+
+
+          <div className='card_row_1'>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+          </div>
+
+
+
+          <div className='card_row_1'>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+            <div className="card_box">
+              <img src={test} alt="Example" />
+              <div className="name_text">
+                <h3>DARLENE <br /> ROBERTSON </h3>
+                <p>OUT IN 2 MIN</p>
+              </div>
+            </div>
+
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+          {/* ############################################################### */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
